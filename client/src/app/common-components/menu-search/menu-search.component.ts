c7c0aeb9353819327bfe506d8035f9c9e4f0340f
bableshaazad <aazadbablesh@gmail.com>
2026-02-05 15:53:47 +0530

    update route and library overview

--- client/src/app/common-components/menu-search/menu-search.component.ts
+++ client/src/app/common-components/menu-search/menu-search.component.ts
@@ -13,7 +13,7 @@ const MENU_ITEMS = [
   { title: 'Desk Of Vice Chancellor', route: 'about/vice-chancellor' },
   { title: 'VC Secretariat', route: "about/Offices/1" },
   { title: 'Registrar Office', route: "about/Offices/2" },
-  { title: 'Comptroller Office', route: "about/Offices/3"},
+  { title: 'Comptroller Office', route: "about/Offices/3" },
   { title: 'Medical Unit', route: "about/Offices/5" },
   { title: 'National Awards', route: "about/awards-achieve/25" },
   { title: 'Student Awards', route: "about/awards-achieve/26" },
@@ -24,17 +24,17 @@ const MENU_ITEMS = [
   { title: 'Photo Gallery', route: "about/gallery/0" },
   { title: 'Seniority List', route: "about/other-news/18" },
   { title: 'Video Gallery', route: "about/video-gallery/0" },
-  { title: 'Facebook', route: "https://www.facebook.com/igkvkisan" },
-  { title: 'Youtube', route: "https://www.youtube.com/playlist?list=UUkzs-8I9dPquYO_e4MEno9A" },
-  { title: 'Instagram', route: "https://www.instagram.com/igkv_kisan/" },
-  { title: 'Twitter', route: "https://twitter.com/igkv_kisan" },
+  { title: 'Facebook', route: "https://www.facebook.com/igkvkisan", external: true },
+  { title: 'Youtube', route: "https://www.youtube.com/playlist?list=UUkzs-8I9dPquYO_e4MEno9A", external: true },
+  { title: 'Instagram', route: "https://www.instagram.com/igkv_kisan/", external: true },
+  { title: 'Twitter', route: "https://twitter.com/igkv_kisan", external: true },
   { title: 'Upcoming events', route: "about/activities/28" },
   { title: 'All Activities', route: "about/activities/all" },
   { title: 'Directorate of Instruction', route: "research/director/111" },
   { title: 'Colleges', route: "about/college-home" },
   { title: 'Departments', route: "about/department" },
   { title: 'Staff', route: "about/staff/0" },
-  { title: 'Alumni', route: "https://igkv.ac.in/alumni/alumnihomepage.aspx" },
+  { title: 'Alumni', route: "https://igkv.ac.in/alumni/alumnihomepage.aspx", external: true },
   { title: 'Placement', route: "about/err404" },
   { title: 'Agriculture Faculty', route: "about/faculty/1" },
   { title: 'Agriculture Engineering Faculty', route: "about/faculty/2" },
@@ -68,8 +68,8 @@ const MENU_ITEMS = [
   { title: 'MOUs', route: "about/other-news/57" },
   { title: 'Directorate of Extension Services', route: "research/director" },
   { title: 'Krishi Vigyan Kendra (KVKs)', route: "research/kvk-home" },
-  { title: 'JAI',   route:"https://igkv.ac.in/jai/jaihome.aspx" },
-  { title: 'LMTdb', route: "https://igkv.ac.in/xenom/index.aspx" },
+  { title: 'JAI', route: "https://igkv.ac.in/jai/jaihome.aspx", external: true },
+  { title: 'LMTdb', route: "https://igkv.ac.in/xenom/index.aspx", external: true },
   { title: 'ATIC', route: "research/atic" },
   { title: 'Kisan Mela', route: "research/kisan-mela" },
   { title: 'Training', route: "research/training/15" },
@@ -95,15 +95,15 @@ const MENU_ITEMS = [
   { title: 'Nehru Library', route: "research/library-overview" },
   { title: 'Director Farm', route: "research/director/112" },
   { title: 'Success Story', route: "research/succesStory" },
-  { title: 'Smart Kisan', route: "/https://igkv.ac.in/smartkisan/Auth/loginpage.aspx" },
+  { title: 'Smart Kisan', route: "https://igkv.ac.in/smartkisan/Auth/loginpage.aspx", external: true },
   { title: 'Poultry', route: "research/farming/1" },
   { title: 'Bee Keeping', route: "research/farming/2" },
   { title: 'Fisheries', route: "research/farming/3" },
   { title: 'Lakh', route: "research/farming/4" },
   { title: 'Marigold', route: "research/farming/5" },
   { title: 'Quail', route: "research/farming/6" },
-  { title: 'Recruitment', route: "https://igkv.ac.in/recruitmenthome.aspx" },
-  { title: 'Admission', route: "https://igkv.ac.in/counselinghomenew.aspx" },
+  { title: 'Recruitment', route: "https://igkv.ac.in/recruitmenthome.aspx", external: true },
+  { title: 'Admission', route: "https://igkv.ac.in/counselinghomenew.aspx", external: true },
   { title: 'Why Choose IGKV', route: "research/international-overview" },
   { title: 'Admission Process', route: "research/international-admission" },
   { title: 'How to apply', route: "research/how-to-apply" },
@@ -143,36 +143,36 @@ const MENU_ITEMS = [
   { title: "College of Agriculture Pratappur (Surajpur)", route: "about/college-home/college/1068" },
   { title: "College of Agriculture Shankargarh (Balrampur)", route: "about/college-home/college/1069" },
 
-  
-    { title: "College of Agriculture, Raipur",route: "research/kvk-home/kvk/1018" },
-    { title: "BTC College Of Agriculture and Research Station,Bilaspur",route: "research/kvk-home/kvk/14" },
-    { title: "Krishi Vigyan Kendra, Ambikapur",route: "research/kvk-home/kvk/16" },
-    { title: "Krishi Vigyan Kendra, Balod",route: "research/kvk-home/kvk/1035" },
-    { title: "Krishi Vigyan Kendra, Balrampur",route: "research/kvk-home/kvk/47" },
-    { title: "Krishi Vigyan Kendra, Bemetara",route: "research/kvk-home/kvk/1030" },
-    { title: "Krishi Vigyan Kendra, Bhatapara",route: "research/kvk-home/kvk/19" },
-    { title: "Krishi Vigyan Kendra, Bijapur",route: "research/kvk-home/kvk/27" },
-    { title: "Krishi Vigyan Kendra, Bilaspur",route: "research/kvk-home/kvk/20" },
-    { title: "Krishi Vigyan Kendra, Dantewada",route: "research/kvk-home/kvk/38" },
-    { title: "Krishi Vigyan Kendra, Dhamatari",route: "research/kvk-home/kvk/18" },
-    { title: "Krishi Vigyan Kendra, Durg",route: "research/kvk-home/kvk/1029" },
-    { title: "Krishi Vigyan Kendra, Gariyaband",route: "research/kvk-home/kvk/48" },
-    { title: "Krishi Vigyan Kendra, Jagdalpur",route: "research/kvk-home/kvk/17" },
-    { title: "Krishi Vigyan Kendra, Janjgir-Champa",route: "research/kvk-home/kvk/21" },
-    { title: "Krishi Vigyan Kendra, Jashpur",route: "research/kvk-home/kvk/25" },
-    { title: "Krishi Vigyan Kendra, Kabirdham",route: "research/kvk-home/kvk/26" },
-    { title: "Krishi Vigyan Kendra, Kanker",route: "research/kvk-home/kvk/24" },
-    { title: "Krishi Vigyan Kendra, Kondagaon",route: "research/kvk-home/kvk/1045" },
-    { title: "Krishi Vigyan Kendra, Korba",route: "research/kvk-home/kvk/23" },
-    { title: "Krishi Vigyan Kendra, Korea",route: "research/kvk-home/kvk/43" },
-    { title: "Krishi Vigyan Kendra, MahaSamund",route: "research/kvk-home/kvk/20" },
-    { title: "Krishi Vigyan Kendra, Mainpat",route: "research/kvk-home/kvk/1033" },
-    { title: "Krishi Vigyan Kendra, Mungeli",route: "research/kvk-home/kvk/1031" },
-    { title: "Krishi Vigyan Kendra, Narayanpur",route: "research/kvk-home/kvk/46" },
-    { title: "Krishi Vigyan Kendra, Raigarh",route: "research/kvk-home/kvk/22" },
-    { title: "Krishi Vigyan Kendra, Raipur",route: "research/kvk-home/kvk/1018" },
-    { title: "Krishi Vigyan Kendra, Rajnandgaon",route: "research/kvk-home/kvk/42" }, 
-    { title: "Krishi Vigyan Kendra, Sukma",route: "research/kvk-home/kvk/1036" }
+
+  { title: "College of Agriculture, Raipur", route: "research/kvk-home/kvk/1018" },
+  { title: "BTC College Of Agriculture and Research Station,Bilaspur", route: "research/kvk-home/kvk/14" },
+  { title: "Krishi Vigyan Kendra, Ambikapur", route: "research/kvk-home/kvk/16" },
+  { title: "Krishi Vigyan Kendra, Balod", route: "research/kvk-home/kvk/1035" },
+  { title: "Krishi Vigyan Kendra, Balrampur", route: "research/kvk-home/kvk/47" },
+  { title: "Krishi Vigyan Kendra, Bemetara", route: "research/kvk-home/kvk/1030" },
+  { title: "Krishi Vigyan Kendra, Bhatapara", route: "research/kvk-home/kvk/19" },
+  { title: "Krishi Vigyan Kendra, Bijapur", route: "research/kvk-home/kvk/27" },
+  { title: "Krishi Vigyan Kendra, Bilaspur", route: "research/kvk-home/kvk/20" },
+  { title: "Krishi Vigyan Kendra, Dantewada", route: "research/kvk-home/kvk/38" },
+  { title: "Krishi Vigyan Kendra, Dhamatari", route: "research/kvk-home/kvk/18" },
+  { title: "Krishi Vigyan Kendra, Durg", route: "research/kvk-home/kvk/1029" },
+  { title: "Krishi Vigyan Kendra, Gariyaband", route: "research/kvk-home/kvk/48" },
+  { title: "Krishi Vigyan Kendra, Jagdalpur", route: "research/kvk-home/kvk/17" },
+  { title: "Krishi Vigyan Kendra, Janjgir-Champa", route: "research/kvk-home/kvk/21" },
+  { title: "Krishi Vigyan Kendra, Jashpur", route: "research/kvk-home/kvk/25" },
+  { title: "Krishi Vigyan Kendra, Kabirdham", route: "research/kvk-home/kvk/26" },
+  { title: "Krishi Vigyan Kendra, Kanker", route: "research/kvk-home/kvk/24" },
+  { title: "Krishi Vigyan Kendra, Kondagaon", route: "research/kvk-home/kvk/1045" },
+  { title: "Krishi Vigyan Kendra, Korba", route: "research/kvk-home/kvk/23" },
+  { title: "Krishi Vigyan Kendra, Korea", route: "research/kvk-home/kvk/43" },
+  { title: "Krishi Vigyan Kendra, MahaSamund", route: "research/kvk-home/kvk/20" },
+  { title: "Krishi Vigyan Kendra, Mainpat", route: "research/kvk-home/kvk/1033" },
+  { title: "Krishi Vigyan Kendra, Mungeli", route: "research/kvk-home/kvk/1031" },
+  { title: "Krishi Vigyan Kendra, Narayanpur", route: "research/kvk-home/kvk/46" },
+  { title: "Krishi Vigyan Kendra, Raigarh", route: "research/kvk-home/kvk/22" },
+  { title: "Krishi Vigyan Kendra, Raipur", route: "research/kvk-home/kvk/1018" },
+  { title: "Krishi Vigyan Kendra, Rajnandgaon", route: "research/kvk-home/kvk/42" },
+  { title: "Krishi Vigyan Kendra, Sukma", route: "research/kvk-home/kvk/1036" }
 
 
 ];
@@ -188,30 +188,41 @@ export class MenuSearchComponent {
   searchQuery: string = ''; // Holds the search input
   filteredMenu = MENU_ITEMS; // All menu items by default
 
-  constructor(private router: Router,public dialogRef: MatDialogRef<MenuSearchComponent>) {}
+  constructor(private router: Router, public dialogRef: MatDialogRef<MenuSearchComponent>) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   // This method filters the menu based on the search query
   onSearch(): void {
     if (this.searchQuery.trim() === '') {
       this.filteredMenu = MENU_ITEMS; // If no search query, show all items
     } else {
-      this.filteredMenu = MENU_ITEMS.filter(menu => 
+      this.filteredMenu = MENU_ITEMS.filter(menu =>
         menu.title.toLowerCase().includes(this.searchQuery.toLowerCase()) // Filter based on query
       );
     }
   }
 
- 
   // This method is called when a user clicks on a menu item to navigate
-  navigateTo(route: string): void {
-    this.router.navigate([route]); // Navigate to the selected route
+  // navigateTo(route: string): void {
+  //   this.router.navigate([route]); // Navigate to the selected route
+  //   this.dialogRef.close();
+  // }
+
+  navigateTo(route: string, external: boolean = false): void {
+    if (external) {
+      // Redirect to old website / external URL
+      window.location.href = route;
+    } else {
+      // Navigate inside Angular app
+      this.router.navigate([route]);
+    }
     this.dialogRef.close();
   }
+
 
   closeDialog(): void {
     this.dialogRef.close();
   }
-  
+
 }
