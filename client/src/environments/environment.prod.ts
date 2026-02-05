fc50f29689a47f9fe4597c9052ecf109e8c0ce20
TushilRawte <tushilrawte100@gmail.com>
2026-02-05 18:16:57 +0530

    added enviroinment file

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,8 +1,24 @@
-let host :string = 'https://igkv.ac.in/site/api/'
+// ^ on live code
+/* let host :string = 'https://igkv.ac.in/site/api/'
 export const environment = {
   production: true,
   i18nPath: '/site/assets/i18n/',
   rootURL:host,
+  // rootURL:'http://localhost:3001/api/',
+  PhotoUrl:'https://igkv.ac.in/images/AngWeb/Photos/',
+  VideoUrl:'https://igkv.ac.in/images/AngWeb/Video/',
+  DocumentUrl:'https://igkv.ac.in/__Files/documents/',
+  Syllabus_Url:'https://igkv.ac.in/__Files/syllabus/', 
+}; */
+
+
+
+// let host :string = 'https://igkv.ac.in/site/api/'
+export const environment = {
+  production: true,
+  i18nPath: '/site/assets/i18n/',
+  // rootURL:host,
+  rootURL: '/site/api/',
   // rootURL:'http://localhost:3001/api/',
   PhotoUrl:'https://igkv.ac.in/images/AngWeb/Photos/',
   VideoUrl:'https://igkv.ac.in/images/AngWeb/Video/',
