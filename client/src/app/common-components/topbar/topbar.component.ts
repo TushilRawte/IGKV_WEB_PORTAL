74fbc435cc0afce3ba6a8be3d0cc8e84ca044a54
TushilRawte <tushilrawte100@gmail.com>
2026-03-17 12:43:49 +0530

    updates in many section

--- client/src/app/common-components/topbar/topbar.component.ts
+++ client/src/app/common-components/topbar/topbar.component.ts
@@ -56,7 +56,7 @@ export class TopbarComponent  {
 
 
   ngOnInit(): void {
-    this.switchLanguage('en')
+    this.switchLanguage('hn')
 this.languageService.currentLanguage$.subscribe(lang => {
 });
     this.route.events.subscribe((val: any) => {
